94d17a8c47fcc01748b2cae2919031176679f713
Sunkenhero <tobi-account_github@tintuc.de>
2023-09-20 16:47:01 +0200

    sd

--- Frontend/src/App2.vue
+++ Frontend/src/App2.vue
@@ -4,6 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,10 +19,20 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style>
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
 nav {
-  width: 150%;
-  font-size: 20px;
+  width: 100%;
+  font-size: 12px;
   margin-top: 2rem;
 }
 
@@ -41,5 +52,24 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
 }
 </style>
