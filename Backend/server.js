7ef81504e64d5b5e6132203e51e22f9819ce2579
Gonzman <yuki.schaefer@googlemail.com>
2023-09-20 21:15:30 +0200

    Added JWT
    Co-authored-by: Tobias <SunkenHero@users.noreply.github.com>

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+//cocaine is fun
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,6 +7,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/login', (req, res) => {
+  res.send('Login')
+});
+
+app.delete('/hausaufgabe/{id}', (req, res) => {
+  res.send('Logout')
+});
+
+app.update('/update', (req, res) => {
+  res.send('Update')
+});
+
+app.put('/put', (req, res) => { 
+  res.send('Put')
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('Listening on port ${port}')
 })
+
+console.log("sui")
