179da6603230cb546b2e3e088b930cf2dff27131
Sunkenhero <tobi-account_github@tintuc.de>
2023-09-20 21:22:35 +0200

    Sui

--- Backend/server.js
+++ Backend/server.js
@@ -1,53 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const auth = require('./auth.js');
-const jwt = require('jsonwebtoken');
-const e = require('express');
+//cocaine is fun
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use(bodyParser.json());
-
-
-app.post('/auth', auth.Auth );
-
-app.use('/api',auth.isAuth);
-app.get('/api', (req, res) => {
-  console.log('protected');
-  res.json({ message: 'API access granted!' });
+app.post('/login', (req, res) => {
+  res.send('Login')
 });
 
-app.use('/api/*', auth.isAuth);
-
-app.get('/api/homeworken', (req, res) => {
-  
+app.delete('/hausaufgabe/{id}', (req, res) => {
+  res.send('Logout')
 });
 
-app.post('/api/homework', (req, res) => {
-  
+app.put('/put', (req, res) => { 
+  res.send('Put')
 });
-
-app.get('/api/homework/:id', (req, res) => {
-  
-});
-
-app.put('/api/homework/:id/', (req, res) => {
- 
-});
-
-app.delete('/api/homework/:id/', (req, res) => {
-  
-});
-
-app.get('/decode', (req, res) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  const decoded = jwt.decode(token);
-  res.json(decoded);
-});
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
+  console.log('Listening on port ' + String(port))
+})
