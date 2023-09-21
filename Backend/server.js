5b5a17ba066c21002911304cc7a1ebd2b553e07f
Gonzman <yuki.schaefer@googlemail.com>
2023-09-21 17:18:19 +0200

    Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,22 +3,50 @@ const bodyParser = require('body-parser');
 const auth = require('./auth.js');
 const jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.post('/login', (req, res) => {
-  res.send('Login')
+app.use(bodyParser.json());
+
+
+app.post('/auth', auth.Auth );
+
+app.use('/api',auth.isAuth);
+app.get('/api', (req, res) => {
+  console.log('protected');
+  res.json({ message: 'API access granted!' });
 });
 
-app.delete('/hausaufgabe/{id}', (req, res) => {
-  res.send('Logout')
+app.use('/api/*', auth.isAuth);
+
+app.get('/api/homeworken', (req, res) => {
+  
 });
 
-app.put('/put', (req, res) => { 
-  res.send('Put')
+app.post('/api/homework', (req, res) => {
+  
 });
+
+app.get('/api/homework/:id', (req, res) => {
+  
+});
+
+app.put('/api/homework/:id/', (req, res) => {
+ 
+});
+
+app.delete('/api/homework/:id/', (req, res) => {
+  
+});
+
+app.get('/decode', (req, res) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  const decoded = jwt.decode(token);
+  res.json(decoded);
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Listening on port ' + String(port))
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
+
